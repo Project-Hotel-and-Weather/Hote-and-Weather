baf6f50449f0b4b5a1855d3fdef8a5fb0f626d77
InieStek <michal001d9@gmail.com>
2022-04-09 09:31:46 +0200

    Initial commit

---
+++ src/main/java/pl/sda/hotelweatherproject/HotelWeatherProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.hotelweatherproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelWeatherProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelWeatherProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/hotelweatherproject/HotelWeatherProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.hotelweatherproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelWeatherProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
