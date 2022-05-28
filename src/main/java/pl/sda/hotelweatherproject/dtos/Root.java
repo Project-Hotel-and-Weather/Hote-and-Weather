0d664165342ca024d0f810ada877ce35edaace68
InieStek <michal001d9@gmail.com>
2022-05-28 14:28:13 +0200

    creating a method to extract the weather

---
+++ src/main/java/pl/sda/hotelweatherproject/dtos/Root.java
@@ -0,0 +1,16 @@
+package pl.sda.hotelweatherproject.dtos;
+
+import java.util.ArrayList;
+
+public class Root{
+
+    public ArrayList<Weather> weather;
+    public String base;
+    public Main main;
+    public int visibility;
+    public int dt;
+    public int timezone;
+    public int id;
+    public String name;
+    public int cod;
+}
