4b0a0f08c2fa20e47561930c19229e11539be43d
InieStek <michal001d9@gmail.com>
2022-06-23 16:20:41 +0200

    improving the test for weather

--- src/test/java/pl/sda/hotelweatherproject/HotelWeatherProjectApplicationTestsModel.java
+++ src/test/java/pl/sda/hotelweatherproject/HotelWeatherProjectApplicationTestsModel.java
@@ -7,6 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -29,30 +32,40 @@ class HotelWeatherProjectApplicationTestsModel {
     @Test
     void findAHotelForCityThatExists() throws Exception {
         mockMvc.perform(get("/hotel")
-                        .param("location", "barcelona")).andDo(print()).andExpect(status().isOk())
+                        .param("location", "barcelona"))
+                .andDo(print())
+                .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"));
     }
 
     @Test
     void notFindAHotelThatExistsAndReturnNull() throws Exception {
         mockMvc.perform(get("/hotel")
-                .param("location","Vladywostok")).andDo(print()).andExpect(status().isOk())
+                .param("location","Vladywostok"))
+                .andDo(print())
+                .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(jsonPath("$.concat(null)").value(""));
     }
 
     @Test
     void findAWeatherForCityThatExists() throws Exception {
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
         mockMvc.perform(get("/weather")
-                        .param("location", "barcelona")).andDo(print()).andExpect(status().isOk())
+                        .param("location", "barcelona"))
+                .andDo(print())
+                .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$.name").value("Sant Pere, Santa Caterina i La Ribera"));
+                .andExpect(jsonPath("$[0].date").value(simpleDateFormat.format(date)));
     }
 
     @Test
     void notFindAWeatherThatExistsAndReturnNull() throws Exception {
         mockMvc.perform(get("/hotel")
-                        .param("location","Vladywostok")).andDo(print()).andExpect(status().isOk())
+                        .param("location","Vladywostok"))
+                .andDo(print())
+                .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(jsonPath("$.concat(null)").value(""));
     }
